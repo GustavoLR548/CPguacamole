aa67468d364335e682d8e22679fee78518e8d257
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-11-11 21:07:36 -0300

    feat: corrigido tarefa 27

--- Tarefa27/distribute_number.c
+++ Tarefa27/distribute_number.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+#define TAG 0
+
 void main(int argc, char* argv[]) {
   int i, rank, val, numProcs;
   MPI_Status status;
@@ -9,16 +11,17 @@ void main(int argc, char* argv[]) {
   MPI_Init(&argc, &argv) ;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+
+  int partner_rank = (rank + 1) % 2;
   if (rank == 0) {
     val = 51;
 
-    MPI_Send(&val, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    MPI_Send(&val, 1, MPI_INT, partner_rank, TAG, MPI_COMM_WORLD);
     printf("Process %d sends a message to %d\n",rank,i);
 
   } else {
 
-    printf("Processor %d receives a message from %d\n", rank,i);
-    MPI_Recv(&val, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+    MPI_Recv(&val, 1, MPI_INT, partner_rank, TAG, MPI_COMM_WORLD, &status);
     printf("Process %d receives a message from %d with value %d\n",rank,0,val);
   }
   MPI_Finalize();
